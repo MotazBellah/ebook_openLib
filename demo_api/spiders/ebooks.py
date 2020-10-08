cc9274bf6e3ab3d18c6b5c738c6d34b041a2a683
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-10-09 01:23:44 +0200

    Scrape openlibrary and get the title and subject of all books and handle the pagination

--- demo_api/spiders/ebooks.py
+++ demo_api/spiders/ebooks.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.exceptions import CloseSpider
+import json
 
 
 class EbooksSpider(scrapy.Spider):
     name = 'ebooks'
-    allowed_domains = ['openlibrary.org/subjects/picture_books.json?limit=12&offset=12']
-    start_urls = ['http://openlibrary.org/subjects/picture_books.json?limit=12&offset=12/']
+
+    INCREMENTED_BY = 12
+    offset = 0
+
+    allowed_domains = ['openlibrary.org']
+    start_urls = ['https://openlibrary.org/subjects/picture_books.json?limit=12']
 
     def parse(self, response):
-        pass
+        if response.status == 500:
+            raise CloseSpider("Reached last page...")
+
+        resp = json.loads(response.body)
+        ebooks = resp.get("works")
+        for ebook in ebooks:
+            yield {
+                'title': ebook.get("title"),
+                'subject': ebook.get("subject"),
+            }
+
+        self.offset += self.INCREMENTED_BY
+        yield scrapy.Request(
+            url=f"https://openlibrary.org/subjects/picture_books.json?limit=12&offset={self.offset}",
+            callback=self.parse
+        )

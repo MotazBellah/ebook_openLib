c70592cd1e575b933f29b68e7abc9180f9d9f770
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-10-09 00:50:04 +0200

    Scrap API

--- demo_api/spiders/quotes.py
+++ demo_api/spiders/quotes.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
 
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/api/quotes?page=1']
 
     def parse(self, response):
-        pass
+        # print(response.body)
+        resp = json.loads(response.body)
+        quotes = resp.get('quotes')
+        for quote in quotes:
+            yield {
+                'author': quote['author']['name'],
+                'tags': quote['tags'],
+                'quote_text': quote['text'],
+            }
